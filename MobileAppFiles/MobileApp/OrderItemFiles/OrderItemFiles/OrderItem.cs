bcfac50e9126472fb6a5b9c48f66405d10bb2d20
Erik Oliva <37450259+olivae@users.noreply.github.com>
2020-07-27 19:55:19 -0400

    Add files via upload

---
+++ MobileAppFiles/MobileApp/OrderItemFiles/OrderItemFiles/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using API_consume.OrderFile;
+using API_consume.ProductFiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_consume.OrderItemFiles
+{
+    public  class OrderItem
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quanitity { get; set; }
+        public string ToppingString { get; set; }
+        public bool Complete { get; set; }
+
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
